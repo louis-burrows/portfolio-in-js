e8ea28273dfc263cf59611188a32e7b71a8b882c
Your Name <you@example.com>
2020-07-27 09:28:38 +0100

    do

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+function dropdown() {
+  document.getElementById('link1').classList.toggle('spinicon');
+  document.getElementById('link2').classList.toggle('spinicon2');
+  document.getElementById('link3').classList.toggle('spinicon3');
+  document.getElementById('link4').classList.toggle('spinicon4');
+
+  
+}
+console.log(dropdown);
+
+
